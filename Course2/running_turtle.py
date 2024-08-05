cc8adb66ed001c4b22b19002e0b78528e8e338f1
Cong Nguyen <levels1912@gmail.com>
2024-08-06 01:23:15 +0700

    first commit

---
+++ Course2/running_turtle.py
@@ -0,0 +1,11 @@
+# Python program to draw star 
+# using Turtle Programming 
+import turtle 
+rua = turtle.Turtle('turtle')
+screen = turtle.Screen()
+
+for i in range(4): 
+    rua.forward(50) 
+    rua.right(90) 
+      
+turtle.done() 
